fe3466e08099a91d876dfdd9e593c5c64af1db86
clearlinewebdesign <clearlinewebdesign@11343cde-aa41-0410-9af0-57f1f6311122>
2008-02-17 17:34:08 +0000

    ws.

--- core/trunk/html-tags/classes/standard/HTMLTags_A.inc.php
+++ core/trunk/html-tags/classes/standard/HTMLTags_A.inc.php
@@ -8,7 +8,10 @@
 require_once PROJECT_ROOT . '/haddock/html-tags/classes/HTMLTags_TagWithContent.inc.php';
 require_once PROJECT_ROOT . '/haddock/html-tags/classes/HTMLTags_URL.inc.php';
 
-class HTMLTags_A extends HTMLTags_TagWithContent
+class
+	HTMLTags_A
+extends
+	HTMLTags_TagWithContent
 {
     public function
         __construct($content = null)
