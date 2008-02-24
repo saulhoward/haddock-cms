2e5f84e05a3b270a24f2b99361b3ff161ab2e927
robert.impey <robert.impey@11343cde-aa41-0410-9af0-57f1f6311122>
2008-02-24 18:33:16 +0000

    Added some styles to the page and re-ordered the page a bit.

--- sites/haddock-cms/trunk/classes/pages/HaddockCMS_HTMLPage.inc.php
+++ sites/haddock-cms/trunk/classes/pages/HaddockCMS_HTMLPage.inc.php
@@ -5,11 +5,93 @@ extends
 	PublicHTML_HTMLPage
 {
 	public function
+		get_head_meta_keywords()
+	{
+		return 'haddock, clearlinewebdesign, PHP, object-oriented, content management system, head quarters';
+	}
+	
+	public function
+		get_head_meta_description()
+	{
+		return 'Head quarters for the Haddock CMS Framework.';
+	}
+	
+	protected function
+		render_head_link_stylesheet()
+	{
+		HTMLTags_LinkRenderer
+			::render_style_sheet_link(
+				'/styles/layout.css'
+			);
+		HTMLTags_LinkRenderer
+			::render_style_sheet_link(
+				'/styles/rounded-corners.css'
+			);
+		
+?>
+<!--[if lt IE 7.]>
+<link href="/styles/gifs-for-ie6.css" rel="stylesheet" type="text/css" />
+<![endif]-->
+<?php
+	}
+	
+	public function
+		render_head_script_javascript()
+	{
+?>
+<script type="text/javascript" src="/scripts/rounded-corners.js"></script>
+<?php
+	}
+	
+	public function
+		render_body()
+	{
+		$this->render_body_tag_open();
+		
+		echo "<div id=\"container\">\n";
+		
+		$this->render_body_div_header();
+		
+		$this->render_body_div_navigation();
+		
+		$this->render_body_div_content();
+		
+		$this->render_body_div_footer();
+		
+		echo "</div>\n";
+		
+		echo "</body>\n";
+	}
+	
+	public function
+		get_body_div_header()
+	{
+		$div_header = parent::get_body_div_header();
+		
+		$div_header->set_attribute_str('class', 'bw-rc');
+		
+		return $div_header;
+	}
+	
+	public function
+		render_body_div_content()
+	{
+?>
+<div id="content" class="bw-rc">
+<?php
+		$this->content();
+?>
+</div>
+<!-- End of content div -->
+<?php
+	}
+	
+	public function
 		render_body_div_navigation()
 	{
 		#echo __METHOD__; exit;
 		
-		echo "<div id=\"navigation\">\n";
+		echo "<div id=\"navigation\" class=\"bw-rc\">\n";
 		
 		#Navigation_SPoE::render_tree('Left Nav');
 		
@@ -26,6 +108,17 @@ extends
 	&copy 2008, Robert Impey
 </div>
 <?php
-	}	
+	}
+	
+	public function
+		render_html()
+	{
+		parent::render_html();
+?>
+<script type="text/javascript">
+round_all_corners('bw-rc');
+</script>
+<?php
+	}
 }
 ?>
