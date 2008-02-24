90bc3a0870fbd38c2fe339cd297fd1869cbe3c04
robert.impey <robert.impey@11343cde-aa41-0410-9af0-57f1f6311122>
2008-02-24 17:10:53 +0000

    Simplified the renderer so that it knows about the right get variable to look at.

--- plug-ins/db-pages/trunk/classes/pages/DBPages_HTMLPage.inc.php
+++ plug-ins/db-pages/trunk/classes/pages/DBPages_HTMLPage.inc.php
@@ -7,11 +7,13 @@ extends
 	public function
 		content()
 	{
-		if (isset($_GET['page'])) {
-			DBPages_PageRenderer::render_page_section($_GET['page'], 'content');
-		} else {
-			echo "<p class=\"error\">Please set the page name!</p>\n";
-		}
+		#if (isset($_GET['page'])) {
+		#	DBPages_PageRenderer::render_page_section($_GET['page'], 'content');
+		#} else {
+		#	echo "<p class=\"error\">Please set the page name!</p>\n";
+		#}
+		
+		DBPages_PageRenderer::render_current_page_content();
 	}
 }
 ?>
--- plug-ins/db-pages/trunk/classes/renderers/DBPages_PageRenderer.inc.php
+++ plug-ins/db-pages/trunk/classes/renderers/DBPages_PageRenderer.inc.php
@@ -16,5 +16,15 @@ class
 	{
 		echo DBPages_SPoE::get_filtered_page_section($page_name, $section_name);
 	}
+	
+	public static function
+		render_current_page_content()
+	{
+		if (isset($_GET['page'])) {
+			DBPages_PageRenderer::render_page_section($_GET['page'], 'content');
+		} else {
+			echo "<p class=\"error\">Please set the page name!</p>\n";
+		}
+	}
 }
 ?>
