3f55526b2e5039f2ef758766ad8650599227cf4c
robert.impey <robert.impey@11343cde-aa41-0410-9af0-57f1f6311122>
2008-03-03 11:39:50 +0000

    Added a method to set the alt attribute of an IMG tag.

--- core/trunk/html-tags/classes/standard/HTMLTags_IMG.inc.php
+++ core/trunk/html-tags/classes/standard/HTMLTags_IMG.inc.php
@@ -5,19 +5,41 @@
  * RFI & SANH 2006-11-29
  */
 
-require_once PROJECT_ROOT . '/haddock/html-tags/classes/HTMLTags_TagWithoutContent.inc.php';
-require_once PROJECT_ROOT . '/haddock/html-tags/classes/HTMLTags_URL.inc.php';
+//require_once PROJECT_ROOT . '/haddock/html-tags/classes/HTMLTags_TagWithoutContent.inc.php';
+//require_once PROJECT_ROOT . '/haddock/html-tags/classes/HTMLTags_URL.inc.php';
 
-class HTMLTags_IMG extends HTMLTags_TagWithoutContent
+class
+	HTMLTags_IMG
+extends
+	HTMLTags_TagWithoutContent
 {
-    public function __construct()
-    {
-        parent::__construct('img');
-    }
-    
-    public function set_src(HTMLTags_URL $href)
-    {
-        $this->set_attribute_str('src', $href->get_as_string());
-    }
+	public function
+		__construct()
+	{
+		parent::__construct('img');
+	}
+	
+	public function
+		set_src(HTMLTags_URL $href)
+	{
+		$this->set_attribute_str('src', $href->get_as_string());
+	}
+	
+	/**
+	 * Set the ALT attribute.
+	 *
+	 * Should this be a requirement for IMG tags?
+	 *
+	 * Are there any other tags that should have ALT set?
+	 *
+	 * How can we share code?
+	 *
+	 * Interfaces and delegation perhaps.
+	 */
+	public function
+		set_alt($alt_text)
+	{
+		$this->set_attribute_str('alt', $alt_text);
+	}
 }
 ?>
