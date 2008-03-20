e2648183efd40ebd204f9b1fa60647e5f866c734
robert.impey <robert.impey@11343cde-aa41-0410-9af0-57f1f6311122>
2008-03-20 16:23:46 +0000

    ws

--- core/trunk/database/classes/sql-statements/Database_SQLSelectQuery.inc.php
+++ core/trunk/database/classes/sql-statements/Database_SQLSelectQuery.inc.php
@@ -2,7 +2,7 @@
 /**
  * Database_SQLSelectQuery
  *
- * @copyright Clear Line Web Design, 2006-11-24
+ * @copyright 2006-11-24, RFI
  */
 
 #require_once PROJECT_ROOT
@@ -18,142 +18,147 @@
 #    . 'Database_AddConditionsToWhereClauseBehaviour.inc.php';
 
 class
-    Database_SQLSelectQuery
+	Database_SQLSelectQuery
 extends
-    Database_SQLStatement
+	Database_SQLStatement
 implements
-    Database_SQLStatementWithWhereClause
+	Database_SQLStatementWithWhereClause
 {
-    private $selected_items_clause;
-    
-    private $tables;
-    
-    private $where_clause;
-    
-    private $order_by_clause;
-    
-    private $limit_clause;
-    
-    protected function
-        set_behaviours()
-    {
-        $this->add_behaviour(
-            'add_conditions_to_where_clause',
-            new Database_AddConditionsToWhereClauseBehaviour($this)
-        );
-    }
-    
-    public function get_selected_items_clause()
-    {
-        if (!isset($this->selected_items_clause)) {
-            $this->selected_items_clause
-                = new Database_SQLStatementSelectedItemsClause();
-        }
-        
-        return $this->selected_items_clause;
-    }
-    
-    public function add_table(Database_Table $table)
-    {
-        if (!is_array($this->tables)) {
-            $this->tables = array();
-        }
-        
-        $this->tables[] = $table;
-    }
-    
-    public function get_where_clause()
-    {
-        if (!isset($this->where_clause)) {
-            $this->where_clause = new Database_SQLStatementWhereClause();
-        }
-        
-        return $this->where_clause;
-    }
-    
-    public function
-        add_conditions_to_where_clause(
-            $conditions
-        )
-    {
-        $this->run_behaviour(
-            'add_conditions_to_where_clause',
-            $conditions
-        );
-    }
-    
-    public function get_order_by_clause()
-    {
-        if (!isset($this->order_by_clause)) {
-            $this->order_by_clause = new Database_SQLStatementsOrderByClause();
-        }
-        
-        return $this->order_by_clause;
-    }
-    
-    public function get_limit_clause()
-    {
-        if (!isset($this->limit_clause)) {
-            $this->limit_clause = new Database_SQLStatementsLimitClause();
-        }
-        
-        return $this->limit_clause;
-    }
-    
-    protected function
-        assemble()
-    {
-        $this->str = 'SELECT ';
-        
-        #$this->str .= ' * ';
-        
-        $selected_items_clause = $this->get_selected_items_clause();
-        
-        $this->str .= $selected_items_clause->get_as_string();
-        
-        $this->str .= ' FROM ';
-        
-        /*
-         * The table references.
-         */
-        if (
-            !is_array($this->tables)
-            and (count($this->tables) < 1)
-        ) {
-            throw new Exception('No table added to SQL SELECT statement!');
-        }
-        
-        $first = TRUE;
-        foreach ($this->tables as $table) {
-            if ($first) {
-                $first = FALSE;
-            } else {
-                $this->str .= ' , ';
-            }
-            
-            $this->str .= ' ' . $table->get_name() . ' ';
-        }
-        
-        /*
-         * The WHERE clause.
-         */
-        $where_clause = $this->get_where_clause();
-        
-        $this->str .= $where_clause->get_as_string();
-        
-        /*
-         * The ORDER BY clause.
-         */
-        $order_by_clause = $this->get_order_by_clause();
-        
-        $this->str .= $order_by_clause->get_as_string();
-        
-        /*
-         * The LIMIT clause.
-         */
-        $limit_clause = $this->get_limit_clause();
-        
-        $this->str .= $limit_clause->get_as_string();
-    }
+	private $selected_items_clause;
+	
+	private $tables;
+	
+	private $where_clause;
+	
+	private $order_by_clause;
+	
+	private $limit_clause;
+	
+	protected function
+		set_behaviours()
+	{
+		$this->add_behaviour(
+			'add_conditions_to_where_clause',
+			new Database_AddConditionsToWhereClauseBehaviour($this)
+		);
+	}
+	
+	public function
+		get_selected_items_clause()
+	{
+		if (!isset($this->selected_items_clause)) {
+			$this->selected_items_clause
+				= new Database_SQLStatementSelectedItemsClause();
+		}
+		
+		return $this->selected_items_clause;
+	}
+	
+	public function
+		add_table(Database_Table $table)
+	{
+		if (!is_array($this->tables)) {
+			$this->tables = array();
+		}
+		
+		$this->tables[] = $table;
+	}
+	
+	public function
+		get_where_clause()
+	{
+		if (!isset($this->where_clause)) {
+			$this->where_clause = new Database_SQLStatementWhereClause();
+		}
+		
+		return $this->where_clause;
+	}
+	
+	public function
+		add_conditions_to_where_clause(
+			$conditions
+		)
+	{
+		$this->run_behaviour(
+			'add_conditions_to_where_clause',
+			$conditions
+		);
+	}
+	
+	public function
+		get_order_by_clause()
+	{
+		if (!isset($this->order_by_clause)) {
+			$this->order_by_clause = new Database_SQLStatementsOrderByClause();
+		}
+		
+		return $this->order_by_clause;
+	}
+	
+	public function
+		get_limit_clause()
+	{
+		if (!isset($this->limit_clause)) {
+			$this->limit_clause = new Database_SQLStatementsLimitClause();
+		}
+		
+		return $this->limit_clause;
+	}
+	
+	protected function
+		assemble()
+	{
+		$this->str = 'SELECT ';
+		
+		#$this->str .= ' * ';
+		
+		$selected_items_clause = $this->get_selected_items_clause();
+		
+		$this->str .= $selected_items_clause->get_as_string();
+		
+		$this->str .= ' FROM ';
+		
+		/*
+		 * The table references.
+		 */
+		if (
+			!is_array($this->tables)
+			and (count($this->tables) < 1)
+		) {
+			throw new Exception('No table added to SQL SELECT statement!');
+		}
+		
+		$first = TRUE;
+		foreach ($this->tables as $table) {
+			if ($first) {
+				$first = FALSE;
+			} else {
+				$this->str .= ' , ';
+			}
+			
+			$this->str .= ' ' . $table->get_name() . ' ';
+		}
+		
+		/*
+		 * The WHERE clause.
+		 */
+		$where_clause = $this->get_where_clause();
+		
+		$this->str .= $where_clause->get_as_string();
+		
+		/*
+		 * The ORDER BY clause.
+		 */
+		$order_by_clause = $this->get_order_by_clause();
+		
+		$this->str .= $order_by_clause->get_as_string();
+		
+		/*
+		 * The LIMIT clause.
+		 */
+		$limit_clause = $this->get_limit_clause();
+		
+		$this->str .= $limit_clause->get_as_string();
+	}
 }
 ?>
--- core/trunk/database/classes/sql-statements/Database_SQLStatement.inc.php
+++ core/trunk/database/classes/sql-statements/Database_SQLStatement.inc.php
@@ -2,7 +2,7 @@
 /**
  * Database_SQLStatement
  *
- * @copyright Clear Line Web Design, 2006-11-18
+ * @copyright 2006-11-18, RFI
  */
 
 /**
@@ -22,127 +22,127 @@
  * a string that can be sent to a database server.
  */
 abstract class
-    Database_SQLStatement
+	Database_SQLStatement
 {
-    /**
-     * This is the string that will be sent to
-     * the database server.
-     */
-    protected $str;
-    
-    /**
-     * Flag to say whether the statement has
-     * been put together to make a string yet.
-     */
-    protected $assembled;
-    
-    private $behaviours;
-    
-    /**
-     * An object can be created as a fully formed string.
-     *
-     * Or you can create an empty statement and then add
-     * items to be assembled later.
-     */
-    public function
-        __construct($str = '')
-    {
-        $this->str = $str;
-        $this->assembled = (strlen($str) > 0);
-        
-        if (!$this->assembled) {
-            $this->behaviours = array();
-            
-            $this->set_behaviours();
-        }
-    }
-    
-    /**
-     * Returns this SQL statement as a string.
-     *
-     * If this statement needs to be put together from
-     * various items, it is.
-     */
-    public final function
-        get_as_string()
-    {
-        if (!$this->assembled) {
-            $this->assemble();
-        }
-        
-        return $this->str;
-    }
-    
-    /**
-     * This puts all the parts together to make a SQL
-     * statement as a string.
-     *
-     * It also sets the "assembled" flag to true.
-     */
-    abstract protected function
-        assemble();
-    
-    /**
-     * This class uses the delegation pattern if
-     * it is not passed a string of the complete
-     * statement via the constructor.
-     *
-     * See
-     *  http://en.wikipedia.org/wiki/Delegation_pattern
-     *
-     * See also the behaviours classes in this
-     * folder.
-     *
-     * This is all cut and pasted from the delegate
-     * row class.
-     * So much for design patterns removing the need
-     * for that sort of thing!
-     */
-    abstract protected function
-        set_behaviours();
-    
-    protected function
-        add_behaviour(
-            $behaviour_name,
-            Database_RowBehaviour $behaviour
-        )
-    {
-        if (array_key_exists($behaviour_name, $this->behaviours)) {
-            throw new Exception(
-                "Unable to set behaviour $behaviour_name more than once!"
-            );
-        } else {
-            $this->behaviours[$behaviour_name] = $behaviour;
-        }
-    }
-    
-    private function
-        get_behaviour($behaviour_name)
-    {
-        if (array_key_exists($behaviour_name, $this->behaviours)) {
-            return $this->behaviours[$behaviour_name];
-        } else {
-            $reflection_class = new ReflectionClass($this);
-            
-            $error_message = $reflection_class->getName();
-            
-            $error_message
-                .= " doesn't have a behaviour called $behaviour_name!";
-            
-            throw new Exception($error_message);
-        }
-    }
-    
-    protected function
-        run_behaviour($behaviour_name)
-    {
-        $behaviour = $this->get_behaviour($behaviour_name);
-        
-        $returned = $behaviour->run();
-        
-        if (isset($returned)) {
-            return $returned;
-        }
-    }
+	/**
+	 * This is the string that will be sent to
+	 * the database server.
+	 */
+	protected $str;
+	
+	/**
+	 * Flag to say whether the statement has
+	 * been put together to make a string yet.
+	 */
+	protected $assembled;
+	
+	private $behaviours;
+	
+	/**
+	 * An object can be created as a fully formed string.
+	 *
+	 * Or you can create an empty statement and then add
+	 * items to be assembled later.
+	 */
+	public function
+		__construct($str = '')
+	{
+		$this->str = $str;
+		$this->assembled = (strlen($str) > 0);
+		
+		if (!$this->assembled) {
+			$this->behaviours = array();
+			
+			$this->set_behaviours();
+		}
+	}
+	
+	/**
+	 * Returns this SQL statement as a string.
+	 *
+	 * If this statement needs to be put together from
+	 * various items, it is.
+	 */
+	public final function
+		get_as_string()
+	{
+		if (!$this->assembled) {
+			$this->assemble();
+		}
+		
+		return $this->str;
+	}
+	
+	/**
+	 * This puts all the parts together to make a SQL
+	 * statement as a string.
+	 *
+	 * It also sets the "assembled" flag to true.
+	 */
+	abstract protected function
+		assemble();
+	
+	/**
+	 * This class uses the delegation pattern if
+	 * it is not passed a string of the complete
+	 * statement via the constructor.
+	 *
+	 * See
+	 *  http://en.wikipedia.org/wiki/Delegation_pattern
+	 *
+	 * See also the behaviours classes in this
+	 * folder.
+	 *
+	 * This is all cut and pasted from the delegate
+	 * row class.
+	 * So much for design patterns removing the need
+	 * for that sort of thing!
+	 */
+	abstract protected function
+		set_behaviours();
+	
+	protected function
+		add_behaviour(
+			$behaviour_name,
+			Database_RowBehaviour $behaviour
+		)
+	{
+		if (array_key_exists($behaviour_name, $this->behaviours)) {
+			throw new Exception(
+				"Unable to set behaviour $behaviour_name more than once!"
+			);
+		} else {
+			$this->behaviours[$behaviour_name] = $behaviour;
+		}
+	}
+	
+	private function
+		get_behaviour($behaviour_name)
+	{
+		if (array_key_exists($behaviour_name, $this->behaviours)) {
+			return $this->behaviours[$behaviour_name];
+		} else {
+			$reflection_class = new ReflectionClass($this);
+			
+			$error_message = $reflection_class->getName();
+			
+			$error_message
+				.= " doesn't have a behaviour called $behaviour_name!";
+			
+			throw new Exception($error_message);
+		}
+	}
+	
+	protected function
+		run_behaviour($behaviour_name)
+	{
+		$behaviour = $this->get_behaviour($behaviour_name);
+		
+		$returned = $behaviour->run();
+		
+		if (isset($returned)) {
+			return $returned;
+		}
+	}
 }
 ?>
