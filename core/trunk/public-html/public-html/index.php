bec7d7d4d9a8eb64726c93beb90e931dfdab25fc
clearlinewebdesign <clearlinewebdesign@11343cde-aa41-0410-9af0-57f1f6311122>
2008-03-27 11:18:39 +0000

    Slightly improved error handling for when there is no passwords file.

--- core/trunk/public-html/public-html/index.php
+++ core/trunk/public-html/public-html/index.php
@@ -5,7 +5,7 @@
  * All requests for haddock pages are rendered by
  * this page.
  *
- * @copyright Clear Line Web Design, 2007-01-03
+ * @copyright 2007-01-03, RFI
  */
 
 #header('Content-Type: text/plain');
@@ -20,10 +20,10 @@
  * the project.
  */
 require_once $_SERVER['DOCUMENT_ROOT']
-    . '/haddock/public-html/public-html/define-include-paths.inc.php';
-    
+	. '/haddock/public-html/public-html/define-include-paths.inc.php';
+	
 require_once $_SERVER['DOCUMENT_ROOT']
-    . '/haddock/public-html/public-html/define-debug-constants.inc.php';
+	. '/haddock/public-html/public-html/define-debug-constants.inc.php';
 
 //require_once PROJECT_ROOT
 //    . '/haddock/public-html/classes/'
@@ -97,69 +97,81 @@ if (isset($_GET['oo-page'])) {
 		exit;
 	}
 } else {
-    $page_manager = PublicHTML_PageManager::get_instance();
-    
-    //echo 'print_r($page_manager)' . "\n";
-    //print_r($page_manager);
-    
-    $page_manager->set_section(
-        isset($_GET['section'])
-            ? $_GET['section'] : NULL
-    );
-    
-    if ($page_manager->get_section() != 'project-specific') {
-        $page_manager->set_module(
-            isset($_GET['module'])
-                ? $_GET['module'] : NULL
-        );
-    }
-    
-    $page_manager->set_page(
-        isset($_GET['page'])
-            ? $_GET['page'] : NULL
-    );
-    
-    $page_manager->set_type(
-        isset($_GET['type'])
-            ? $_GET['type'] : NULL
-    );
-    
-    //echo 'print_r($page_manager)' . "\n";
-    //print_r($page_manager);
-    
-    //echo '$page_manager->get_section(): ' . $page_manager->get_section() . "\n";
-    //
-    //if ($page_manager->get_section() != 'project-specific') {
-    //    echo '$page_manager->get_module(): ' . $page_manager->get_module() . "\n";
-    //}
-    //
-    //echo '$page_manager->get_page(): ' . $page_manager->get_page() . "\n";
-    //echo '$page_manager->get_type(): ' . $page_manager->get_type() . "\n";
-    //exit;
-    
-    if ($page_manager->is_page()) {
-        //echo "Page found!\n";
-        //exit;
-    } else {
-        #echo "Page not found!\n";
-        
-        $page_manager->set_section('haddock');
-        $page_manager->set_module('public-html');
-        $page_manager->set_page('page-not-found');
-        $page_manager->set_type('html');
-    }
-    
-    //echo '$page_manager->get_section(): ' . $page_manager->get_section() . "\n";
-    //
-    //if ($page_manager->get_section() != 'project-specific') {
-    //    echo '$page_manager->get_module(): ' . $page_manager->get_module() . "\n";
-    //}
-    //
-    //echo '$page_manager->get_page(): ' . $page_manager->get_page() . "\n";
-    //echo '$page_manager->get_type(): ' . $page_manager->get_type() . "\n";
-    //exit;
-    
-    $page_manager->render_inc_file('complete-page');
+	/*
+	 * The "old-fashioned" way.
+	 *
+	 * It turns out that writing .INC files and relying on the
+	 * page manager to find them is incredibly difficult.
+	 *
+	 * My recommendation would be not to write any new code that
+	 * gets called from here and use the OO pages instead.
+	 *
+	 * This probably won't be removed for quite a while, however.
+	 */
+	
+	$page_manager = PublicHTML_PageManager::get_instance();
+	
+	//echo 'print_r($page_manager)' . "\n";
+	//print_r($page_manager);
+	
+	$page_manager->set_section(
+		isset($_GET['section'])
+			? $_GET['section'] : NULL
+	);
+	
+	if ($page_manager->get_section() != 'project-specific') {
+		$page_manager->set_module(
+			isset($_GET['module'])
+				? $_GET['module'] : NULL
+		);
+	}
+	
+	$page_manager->set_page(
+		isset($_GET['page'])
+			? $_GET['page'] : NULL
+	);
+	
+	$page_manager->set_type(
+		isset($_GET['type'])
+			? $_GET['type'] : NULL
+	);
+	
+	//echo 'print_r($page_manager)' . "\n";
+	//print_r($page_manager);
+	
+	//echo '$page_manager->get_section(): ' . $page_manager->get_section() . "\n";
+	//
+	//if ($page_manager->get_section() != 'project-specific') {
+	//    echo '$page_manager->get_module(): ' . $page_manager->get_module() . "\n";
+	//}
+	//
+	//echo '$page_manager->get_page(): ' . $page_manager->get_page() . "\n";
+	//echo '$page_manager->get_type(): ' . $page_manager->get_type() . "\n";
+	//exit;
+	
+	if ($page_manager->is_page()) {
+		//echo "Page found!\n";
+		//exit;
+	} else {
+		#echo "Page not found!\n";
+		
+		$page_manager->set_section('haddock');
+		$page_manager->set_module('public-html');
+		$page_manager->set_page('page-not-found');
+		$page_manager->set_type('html');
+	}
+	
+	//echo '$page_manager->get_section(): ' . $page_manager->get_section() . "\n";
+	//
+	//if ($page_manager->get_section() != 'project-specific') {
+	//    echo '$page_manager->get_module(): ' . $page_manager->get_module() . "\n";
+	//}
+	//
+	//echo '$page_manager->get_page(): ' . $page_manager->get_page() . "\n";
+	//echo '$page_manager->get_type(): ' . $page_manager->get_type() . "\n";
+	//exit;
+	
+	$page_manager->render_inc_file('complete-page');
 }
 
 ob_end_flush();
