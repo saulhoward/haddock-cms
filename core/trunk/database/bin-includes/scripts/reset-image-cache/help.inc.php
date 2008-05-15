1e87ff9fc91331302d914d7cd65b23e48c66430a
clearlinewebdesign <clearlinewebdesign@11343cde-aa41-0410-9af0-57f1f6311122>
2008-05-15 15:51:57 +0000

    Added a little CLI script to reset the image cache.

---
+++ core/trunk/database/bin-includes/scripts/reset-image-cache/help.inc.php
@@ -0,0 +1,2 @@
+The reset-image-cache script.
+© 2008-05-15, Robert Impey
---
+++ core/trunk/database/bin-includes/scripts/reset-image-cache/main.inc.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * The main .INC for the reset-image-cache script.
+ *
+ * @copyright 2008-05-15, Robert Impey
+ */
+
+Database_ImageCacheHelper
+	::reset_image_cache();
+?>
