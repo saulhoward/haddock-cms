0c7f561b0b655b2397aae7570b41b127774756c6
clearlinewebdesign <clearlinewebdesign@11343cde-aa41-0410-9af0-57f1f6311122>
2008-05-27 16:18:00 +0000

    Added a CLI script for running all the unit tests.

---
+++ core/trunk/unit-tests/classes/UnitTests_TestResultsSet.inc.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * UnitTests_TestResultsSet
+ *
+ * @copyright 2008-05-27, RFI
+ */
+
+class
+	UnitTests_TestResultsSet
+{
+	private $test_results;
+	
+	public function
+		__construct()
+	{
+		$this->test_results = array();
+	}
+	
+	public function
+		get_test_results()
+	{
+		return $this->test_results;
+	}
+	
+	public function
+		add_test_result(
+			$test_result
+		)
+	{
+		$this->test_results[] = $test_result;
+	}
+	
+	public function
+		get_summary()
+	{
+		$summary = array();
+		
+		$summary['total_passed_tests'] = 0;
+		$summary['total_test_functions'] = 0;
+		$summary['all_tests_passed'] = TRUE;
+		$summary['total_test_time'] = 0.0;
+		
+		foreach (
+			$this->get_test_results()
+			as
+			$test_result
+		) {
+			$summary['total_passed_tests']
+				+= $test_result['passed_tests_count'];
+			$summary['total_test_functions']
+				+= $test_result['test_functions_count'];
+			$summary['all_tests_passed']
+				= $summary['all_tests_pass'] && $test_result['passes_all_tests'];
+			$summary['total_test_time']
+				+= $test_result['total_test_time'];
+		}
+		
+		return $summary;
+	}
+}
+?>
---
+++ core/trunk/unit-tests/classes/helpers/UnitTests_TestsHelper.inc.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * UnitTests_TestsHelper
+ *
+ * @copyright 2008-05-24, RFI
+ */
+
+class
+	UnitTests_TestsHelper
+{
+	/**
+	 * Runs all the tests in the project and returns an assoc
+	 * with data about the test results.
+	 */
+	public static function
+		run_all_tests()
+	{
+		#$test_results = array();
+		$test_results_set = new UnitTests_TestResultsSet();
+		
+		$unit_tests_php_class_files
+			= self
+				::get_all_unit_tests_php_class_files();
+		
+		foreach (
+			$unit_tests_php_class_files
+			as
+			$unit_tests_php_class_file
+		) {
+			$test_result = array();
+			
+			$test_result['test_name']
+				= $unit_tests_php_class_file->get_test_name();
+			
+			$test_result['passed_tests_count']
+				= $unit_tests_php_class_file->count_passed_tests();
+			
+			$test_result['test_functions_count']
+				= $unit_tests_php_class_file->count_test_functions();
+			
+			$test_result['passes_all_tests']
+				= $unit_tests_php_class_file->passes_all_tests();
+			
+			$test_result['total_test_time']
+				= $unit_tests_php_class_file->get_total_test_time();
+			
+			#$test_results[] = $test_result;
+			$test_results_set->add_test_result($test_result);
+		}
+		
+		#return $test_results;
+		return $test_results_set;
+	}
+	
+	public static function
+		get_all_unit_tests_php_class_files()
+	{
+		$project_directory_finder
+			= HaddockProjectOrganisation_ProjectDirectoryFinder::get_instance();
+		
+		$project_directory
+			= $project_directory_finder->get_project_directory_for_this_project();
+		
+		return
+			$project_directory
+				->get_all_unit_tests_php_class_files();
+	}
+}
+?>
