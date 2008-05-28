51b3a1112ce99f0de00d9ca5cec4647c706ff166
robert.impey <robert.impey@11343cde-aa41-0410-9af0-57f1f6311122>
2008-05-28 20:21:50 +0000

    Added a test for checking that the vhost is reachable.

---
+++ core/trunk/public-html/classes/unit-tests/PublicHTML_VHostTests.inc.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * PublicHTML_VHostTests
+ *
+ * @copyright 2008-05-28, RFI
+ */
+
+class
+	PublicHTML_VHostTests
+extends
+	UnitTests_UnitTests
+{	
+	public static function
+		test_vhost_is_reachable_on_server()
+	{
+		$ph_cm
+			= Configuration_ConfigManagerHelper
+				::get_config_manager(
+					'haddock',
+					'public-html'
+				);
+		
+		$server_address = $ph_cm->get_server_address();
+		
+		$ch = curl_init();
+		
+		curl_setopt($ch, CURLOPT_URL, $server_address);
+		curl_setopt($ch, CURLOPT_HEADER, TRUE);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		
+		$out = curl_exec($ch);
+		
+		curl_close($ch);
+		
+		#echo $out;
+		
+		$lines = Strings_SplittingHelper::split_by_eol($out);
+		
+		if (
+			preg_match(
+				'{^HTTP/[\d.]+ (\d+)}',
+				$lines[0],
+				$matches
+			)
+		) {
+			#print_r($matches);
+			
+			$http_response_code = $matches[1];
+			
+			#echo __METHOD__ . PHP_EOL;
+			#echo "\$http_response_code: $http_response_code" . PHP_EOL;
+			
+			if (
+				in_array(
+					$http_response_code,
+					explode(
+						' ',
+						'200 301 302'
+					)
+				)
+			) {
+				/*
+				 * The server returned the code for 'OK', 'Moved Permanently'
+				 * or 'Temp'.
+				 *
+				 * see http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+				 */
+				
+				return TRUE;
+			}
+		}
+		
+		return FALSE;
+	}
+}
+?>
