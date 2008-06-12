6312d5c1277d06d422c5712940b655f66f578f53
robert.impey <robert.impey@11343cde-aa41-0410-9af0-57f1f6311122>
2008-06-12 10:23:37 +0000

    Added tests for the table specification system.
    At this point they all fail as the system has not been implemented yet.

---
+++ core/trunk/database/classes/unit-tests/Database_TableSpecificationTests.inc.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Database_TableSpecificationTests
+ *
+ * @copyright 2008-06-12, RFI
+ */
+
+class
+	Database_TableSpecificationTests
+extends
+	UnitTests_UnitTests
+{
+	public static function
+		test_all_specified_tables_exist_in_db()
+	{
+		return FALSE;
+	}
+	
+	public static function
+		test_all_db_tables_exist_in_specification()
+	{
+		return FALSE;
+	}
+
+	public static function
+		test_all_specified_fields_exist_in_db()
+	{
+		return FALSE;
+	}
+
+	public static function
+		test_all_db_fields_exist_in_specification()
+	{
+		return FALSE;
+	}
+	
+	public static function
+		test_all_specified_fields_have_correct_type_in_db()
+	{
+		return FALSE;
+	}
+
+	public static function
+		test_all_db_fields_have_correct_type_in_specification()
+	{
+		return FALSE;
+	}
+	public static function
+		test_all_specified_keys_exist_in_db()
+	{
+		return FALSE;
+	}
+
+	public static function
+		test_all_db_keys_exist_in_specification()
+	{
+		return FALSE;
+	}
+	
+	public static function
+		test_all_specified_keys_have_correct_type_in_db()
+	{
+		return FALSE;
+	}
+
+	public static function
+		test_all_db_keys_have_correct_type_in_specification()
+	{
+		return FALSE;
+	}
+}
+?>
