aea5055679353bbe91b477d63997df990aca77bc
robert.impey <robert.impey@11343cde-aa41-0410-9af0-57f1f6311122>
2008-08-09 08:09:25 +0000

    Removed a CLI script for resetting a user's password that has never been implemented.

--- core/trunk/admin/bin-includes/scripts/reset-users-password/args.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * The args for the reset-users-password script.
- *
- * @copyright Clear Line Web Design, 2007-09-19
- */
-?>
--- core/trunk/admin/bin-includes/scripts/reset-users-password/help.inc.php
+++
@@ -1,2 +0,0 @@
-The reset-users-password script.
-© Clear Line Web Design, 2007-09-19
