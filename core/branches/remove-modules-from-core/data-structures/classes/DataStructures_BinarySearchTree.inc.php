2c7c608d359d323b4ebad94433ef6fa363b04eed
robert.impey <robert.impey@11343cde-aa41-0410-9af0-57f1f6311122>
2008-08-22 03:16:47 +0000

    Tidied up comments and white space in the data-structures module.

--- core/branches/remove-modules-from-core/data-structures/classes/DataStructures_BinarySearchTree.inc.php
+++ core/branches/remove-modules-from-core/data-structures/classes/DataStructures_BinarySearchTree.inc.php
@@ -2,16 +2,8 @@
 /**
  * DataStructures_BinarySearchTree
  *
- * @copyright Clear Line Web Design, 2007-03-30
+ * @copyright 2007-03-30, Robert Impey
  */
-
-/*
- * Define the necessary classes.
- */
-
-require_once PROJECT_ROOT
-    . '/haddock/data-structures/classes/'
-    . 'DataStructures_BSTNode.inc.php';
 
 /**
  * An implementation of a binary search tree.
@@ -35,92 +27,92 @@ require_once PROJECT_ROOT
  * to do or the value was towards to the end of the array.
  */
 class
-    DataStructures_BinarySearchTree
+	DataStructures_BinarySearchTree
 {
-    private $root_node;
-    
-    public function
-        __construct()
-    {
-        $this->root_node = NULL;
-    }
-    
-    public function
-        insert_name_value_pair($name, $value)
-    {
-        if (!isset($this->root_node)) {
-            $this->root_node = new DataStructures_BSTNode($name, $value);
-        } else {
-            $new_node = new DataStructures_BSTNode($name, $value);
-            
-            $this->root_node->insert($new_node);
-        }
-    }
-    
-    public function
-        insert_assoc($assoc, $shuffle_before_insert = TRUE)
-    {
-        $keys = array_keys($assoc);
-        
-        if ($shuffle_before_insert) {
-            shuffle($keys);
-        }
-        
-        foreach ($keys as $key) {
-            $this->insert_name_value_pair($key, $assoc[$key]);
-        }
-    }
-    
-    public static function
-        get_bst_for_assoc($assoc, $shuffle_before_insert = TRUE)
-    {
-        $bst = new DataStructures_BinarySearchTree();
-        
-        $bst->insert_assoc($assoc, $shuffle_before_insert);
-        
-        return $bst;
-    }
-    
-    public function
-        get_lowest_value()
-    {
-        $lowest_node = $this->root_node->get_lowest_node();
-        return $lowest_node->get_value();
-    }
-    
-    public function
-        get_greatest_value()
-    {
-        $greatest_node = $this->root_node->get_greatest_node();
-        return $greatest_node->get_value();
-    }
-    
-    public function
-        get_name_with_lowest_value()
-    {
-        $lowest_node = $this->root_node->get_lowest_node();
-        return $lowest_node->get_name();
-    }
-    
-    public function
-        get_name_with_greatest_value()
-    {
-        $greatest_node = $this->root_node->get_greatest_node();
-        return $greatest_node->get_name();
-    }
-    
-    public function
-        get_closest_value($value)
-    {
-        $closest_node = $this->root_node->get_closest_node($value);
-        return $closest_node->get_value();
-    }
-    
-    public function
-        get_name_with_closest_value($value)
-    {
-        $closest_node = $this->root_node->get_closest_node($value);
-        return $closest_node->get_name();
-    }
+	private $root_node;
+	
+	public function
+		__construct()
+	{
+		$this->root_node = NULL;
+	}
+	
+	public function
+		insert_name_value_pair($name, $value)
+	{
+		if (!isset($this->root_node)) {
+			$this->root_node = new DataStructures_BSTNode($name, $value);
+		} else {
+			$new_node = new DataStructures_BSTNode($name, $value);
+			
+			$this->root_node->insert($new_node);
+		}
+	}
+	
+	public function
+		insert_assoc($assoc, $shuffle_before_insert = TRUE)
+	{
+		$keys = array_keys($assoc);
+		
+		if ($shuffle_before_insert) {
+			shuffle($keys);
+		}
+		
+		foreach ($keys as $key) {
+			$this->insert_name_value_pair($key, $assoc[$key]);
+		}
+	}
+	
+	public static function
+		get_bst_for_assoc($assoc, $shuffle_before_insert = TRUE)
+	{
+		$bst = new DataStructures_BinarySearchTree();
+		
+		$bst->insert_assoc($assoc, $shuffle_before_insert);
+		
+		return $bst;
+	}
+	
+	public function
+		get_lowest_value()
+	{
+		$lowest_node = $this->root_node->get_lowest_node();
+		return $lowest_node->get_value();
+	}
+	
+	public function
+		get_greatest_value()
+	{
+		$greatest_node = $this->root_node->get_greatest_node();
+		return $greatest_node->get_value();
+	}
+	
+	public function
+		get_name_with_lowest_value()
+	{
+		$lowest_node = $this->root_node->get_lowest_node();
+		return $lowest_node->get_name();
+	}
+	
+	public function
+		get_name_with_greatest_value()
+	{
+		$greatest_node = $this->root_node->get_greatest_node();
+		return $greatest_node->get_name();
+	}
+	
+	public function
+		get_closest_value($value)
+	{
+		$closest_node = $this->root_node->get_closest_node($value);
+		return $closest_node->get_value();
+	}
+	
+	public function
+		get_name_with_closest_value($value)
+	{
+		$closest_node = $this->root_node->get_closest_node($value);
+		return $closest_node->get_name();
+	}
 }
 ?>
