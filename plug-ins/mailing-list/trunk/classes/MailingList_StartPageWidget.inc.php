d8634e4db6e560295fe5f28fd529cfa0d02ab788
saulhoward <saulhoward@11343cde-aa41-0410-9af0-57f1f6311122>
2008-10-11 11:27:16 +0000

    cleaned up widget class

--- plug-ins/mailing-list/trunk/classes/MailingList_StartPageWidget.inc.php
+++ plug-ins/mailing-list/trunk/classes/MailingList_StartPageWidget.inc.php
@@ -5,35 +5,16 @@ class
 extends
 	Admin_StartPageWidget
 {
-
-	public function
+	protected function
 		get_widget_title()
 	{
 		return 'Mailing List';
 	}
 
-	public function
+	protected function
 		get_widget_content()
 	{
 		return MailingList_PeopleHelper::get_widget_content();
 	}
-
-	public function
-		get_widget_div()
-	{
-		$div = new HTMLTags_Div();
-		$div->set_attribute_str('class', 'StartPageWidget');
-
-		$heading = new HTMLTags_Heading(3, self::get_widget_title());
-		$div->append($heading);
-
-		$div->append(self::get_widget_content());
-
-		return $div;
-	}
-
 }
-
-
-
 ?>
