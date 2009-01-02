a24e0c9333abea344198953c99fee6d0000f3dcb
robert.impey <robert.impey@11343cde-aa41-0410-9af0-57f1f6311122>
2009-01-02 15:52:54 +0000

    Added a script to set the default location for a project.

---
+++ plug-ins/public-html/trunk/classes/cli-scripts/PublicHTML_SetDefaultLocationCLIScript.inc.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * PublicHTML_SetDefaultLocationCLIScript
+ *
+ * @copyright 2009-01-03, Robert Impey
+ */
+
+class
+	PublicHTML_SetDefaultLocationCLIScript
+extends
+	CLIScripts_CLIScript
+{
+	public function
+		do_actions()
+	{
+		/*
+		 * Write code here.
+		 */
+	}
+}
+?>
