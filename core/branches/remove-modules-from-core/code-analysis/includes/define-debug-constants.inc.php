9b8d3dfb5f56e7b3a4f61ad9c7eb1e3291881242
robert.impey <robert.impey@11343cde-aa41-0410-9af0-57f1f6311122>
2009-01-02 14:37:02 +0000

    Made the debug toggling a little safer by moving it out of the code.

--- core/branches/remove-modules-from-core/code-analysis/includes/define-debug-constants.inc.php
+++ core/branches/remove-modules-from-core/code-analysis/includes/define-debug-constants.inc.php
@@ -1,19 +1,42 @@
 <?php
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Defines the debug constants for the project.
+ *
+ * @copyright 2009-01-02, Robert Impey
  */
 
 /*
- * Uncomment the following line to get lots of debug info.
+ * To print lots of debug info (perhaps too much!)
+ * create a file called
+ *
+ * 	PROJECT_ROOT . '/DEBUG.inc.php'
+ *
+ * 	and add the following content:
+ *
+ * 	<?php define('DEBUG', TRUE); ?>
+ *
+ * 	If you are using a version control system, it's probably
+ * 	a good idea to get the system to ignore the debug toggle file.
  */
-#define('DEBUG', TRUE);
+
+$instance_specific_debug_toggle_file_name = PROJECT_ROOT . '/DEBUG.inc.php';
+
+if (file_exists($instance_specific_debug_toggle_file_name)) {
+	require_once $instance_specific_debug_toggle_file_name;
+}
 
 if (!defined('DEBUG')) {
 	define('DEBUG', FALSE);
 }
 
-define('DEBUG_DELIM_OPEN', "\nDEBUG >>>>>>>>>>>>>>>>\n\n");
-define('DEBUG_DELIM_CLOSE', "\nDEBUG <<<<<<<<<<<<<<<<\n\n");
+define(
+	'DEBUG_DELIM_OPEN',
+	PHP_EOL . 'DEBUG >>>>>>>>>>>>>>>>' . PHP_EOL . PHP_EOL
+);
+
+define(
+	'DEBUG_DELIM_CLOSE',
+	PHP_EOL . 'DEBUG <<<<<<<<<<<<<<<<' . PHP_EOL . PHP_EOL
+);
 
 ?>
