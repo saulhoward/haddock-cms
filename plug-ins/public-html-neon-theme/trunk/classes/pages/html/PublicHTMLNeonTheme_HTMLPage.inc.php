f537b1859134ed989f61439def94a6cb55d83be6
robert.impey <robert.impey@11343cde-aa41-0410-9af0-57f1f6311122>
2009-02-02 14:25:05 +0000

    Added the neon them HTML page class.

---
+++ plug-ins/public-html-neon-theme/trunk/classes/pages/html/PublicHTMLNeonTheme_HTMLPage.inc.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * PublicHTMLNeonTheme_HTMLPage
+ *
+ * @copyright 2009-02-02, Robert Impey
+ */
+
+abstract class
+	PublicHTMLNeonTheme_HTMLPage
+extends
+	PublicHTML_HTMLPage
+{
+}
+?>
