db16a4ac99868db1cfb4518c051fd51f6a325710
robert.impey <robert.impey@11343cde-aa41-0410-9af0-57f1f6311122>
2009-02-03 02:20:18 +0000

    Imported a lot of style files from the Haddock CMS HQ site.

--- plug-ins/public-html-sky-theme/trunk/classes/pages/html/PublicHTMLSkyTheme_HTMLPage.inc.php
+++ plug-ins/public-html-sky-theme/trunk/classes/pages/html/PublicHTMLSkyTheme_HTMLPage.inc.php
@@ -10,21 +10,71 @@ abstract class
 extends
 	PublicHTML_HTMLPage
 {
+	
 	protected function
 		render_head_link_stylesheet()
 	{
 		HTMLTags_LinkRenderer
 			::render_style_sheet_link(
-				'http://yui.yahooapis.com/2.3.1/build/reset-fonts-grids/reset-fonts-grids.css'
+				'/plug-ins/public-html-sky-theme/public-html/styles/layout.css'
 			);
 		HTMLTags_LinkRenderer
 			::render_style_sheet_link(
-				'http://yui.yahooapis.com/2.3.1/build/base/base-min.css'
+				'/plug-ins/public-html-sky-theme/public-html/styles/type-face.css'
 			);
 		HTMLTags_LinkRenderer
 			::render_style_sheet_link(
-				'/plug-ins/public-html-sky-theme/public-html/styles/styles.css'
+				'/plug-ins/public-html-sky-theme/public-html/styles/rounded-corners.css'
 			);
+		
+?>
+<!--[if lt IE 7.]>
+<link href="/plug-ins/public-html-sky-theme/public-html/styles/gifs-for-ie6.css" rel="stylesheet" type="text/css" />
+<![endif]-->
+<?php
+	}
+		
+	public function
+		render_head_script_javascript()
+	{
+		HTMLTags_ScriptRenderer
+			::render_external_js_script(
+				'/plug-ins/public-html-sky-theme/public-html/scripts/rounded-corners.js'
+			);
+	}
+	
+	public function
+		get_body_div_header()
+	{
+		$div_header = parent::get_body_div_header();
+		
+		$div_header->set_attribute_str('class', 'bw-rc');
+		
+		return $div_header;
+	}
+	
+	public function
+		render_body_div_content()
+	{
+?>
+<div id="content" class="bw-rc">
+<?php
+		$this->content();
+?>
+</div>
+<!-- End of content div -->
+<?php
+	}
+	
+	public function
+		render_html()
+	{
+		parent::render_html();
+?>
+<script type="text/javascript">
+round_all_corners('bw-rc');
+</script>
+<?php
 	}
 }
 ?>
