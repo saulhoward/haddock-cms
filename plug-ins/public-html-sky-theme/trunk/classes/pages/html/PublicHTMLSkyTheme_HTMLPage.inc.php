3ca51e30be85845cd770913cf199542f5132e4a5
robert.impey <robert.impey@11343cde-aa41-0410-9af0-57f1f6311122>
2009-02-03 01:47:11 +0000

    Added an empty style sheet for the sky theme.

--- plug-ins/public-html-sky-theme/trunk/classes/pages/html/PublicHTMLSkyTheme_HTMLPage.inc.php
+++ plug-ins/public-html-sky-theme/trunk/classes/pages/html/PublicHTMLSkyTheme_HTMLPage.inc.php
@@ -10,5 +10,21 @@ abstract class
 extends
 	PublicHTML_HTMLPage
 {
+	protected function
+		render_head_link_stylesheet()
+	{
+		HTMLTags_LinkRenderer
+			::render_style_sheet_link(
+				'http://yui.yahooapis.com/2.3.1/build/reset-fonts-grids/reset-fonts-grids.css'
+			);
+		HTMLTags_LinkRenderer
+			::render_style_sheet_link(
+				'http://yui.yahooapis.com/2.3.1/build/base/base-min.css'
+			);
+		HTMLTags_LinkRenderer
+			::render_style_sheet_link(
+				'/plug-ins/public-html-sky-theme/public-html/styles/styles.css'
+			);
+	}
 }
 ?>
