3a1f4199d98b9ba4a3870b6b23240c9958a22da3
saulhoward <saulhoward@11343cde-aa41-0410-9af0-57f1f6311122>
2009-05-08 13:22:48 +0000

    sped up img src fetcher if filetype already known

--- core/trunk/database/classes/helpers/Database_ImagesHelper.inc.php
+++ core/trunk/database/classes/helpers/Database_ImagesHelper.inc.php
@@ -8,23 +8,29 @@
 class
 	Database_ImagesHelper
 {
+
 	public static function
-		get_img_src($image_id)
+		get_img_src($image_id, $file_type = NULL)
 	{
-		$mysql_user_factory = Database_MySQLUserFactory::get_instance();
+		if (
+			($file_type)
+			&&
+			($file_type == 'image/jpeg')
+		) {
+			$src = '/hc-database-img-cache/' . $image_id . '.jpg';
+		} else {
 
-		$mysql_user = $mysql_user_factory->get_for_this_project();
-		$database = $mysql_user->get_database();
+			$mysql_user_factory = Database_MySQLUserFactory::get_instance();
+			$mysql_user = $mysql_user_factory->get_for_this_project();
+			$database = $mysql_user->get_database();
+			$images_table = $database->get_table('hc_database_images');
+			$image_row = $images_table->get_row_by_id($image_id);
+			$image_renderer = $image_row->get_renderer();
 
-		$images_table = $database->get_table('hc_database_images');
-
-		$image_row = $images_table->get_row_by_id($image_id);
-
-		$image_renderer = $image_row->get_renderer();
-
-		$url = $image_renderer->get_html_url_in_public_images();
-
-		return $url->get_file();
+			$url = $image_renderer->get_html_url_in_public_images();
+			$src = $url->get_file();
+		}
+		return $src;
 	}
 	
 	public static function
