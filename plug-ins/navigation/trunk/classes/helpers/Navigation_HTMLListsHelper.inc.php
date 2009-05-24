0a3843180ed369ab1435e0a3a63518bfe3b2b4bc
saulhoward <saulhoward@11343cde-aa41-0410-9af0-57f1f6311122>
2009-05-24 11:45:46 +0000

    added new func with support for selected lines

--- plug-ins/navigation/trunk/classes/helpers/Navigation_HTMLListsHelper.inc.php
+++ plug-ins/navigation/trunk/classes/helpers/Navigation_HTMLListsHelper.inc.php
@@ -54,6 +54,108 @@ class
 		
 		return $ul;
 	}
+
+	public static function
+		get_1d_ul_with_selected_lines(
+			$tree_name,
+			$class_name = NULL,
+			$page_class_str = NULL
+		)
+	{
+		if (!isset($class_name)) {
+			$class_name = 'navigation';
+		}
+		
+		$nodes
+			= Navigation_1DTreeRetriever
+				::get_tree_nodes($tree_name);
+				
+		#print_r($nodes);
+		
+		#echo "<ul class=\"$class_name\">\n";
+		
+		$ul = new HTMLTags_UL();
+		
+		$ul->set_class($class_name);
+		
+		foreach ($nodes as $node) {
+			#Navigation_NodeRenderer::render_node($node);
+			
+			$li = new HTMLTags_LI();
+
+			if (
+				($page_class_str != NULL)
+				&& 
+				(self::is_node_selected($node, $page_class_str))
+			)	{
+				$li->set_class('selected');
+			}
+			
+			$li->append(
+				Navigation_NodesHelper
+					::get_link_a($node)
+			);
+			
+			$ul->add_li(
+				$li
+			);
+		}
+		
+		return $ul;
+	}
+
+	public static function 
+		is_node_selected(
+			$node,
+			$page_class_str
+		)
+	{
+		//print_r($node);print_r($page_class_str);exit;
+		//print_r($node);print_r($page_class_str);
+
+                /*
+		 *Home Page, if it's the Default page and the href is '/'
+                 */
+		if ($node['url_href'] == '/') {
+			$cmf = HaddockProjectOrganisation_ConfigManagerFactory::get_instance();
+			$pm = $cmf->get_config_manager('haddock', 'public-html');
+			if ('/' . $page_class_str == $pm->get_default_url()) {
+				return TRUE;
+			}
+		}
+
+                /*
+		 *DB Page, if the url starts with '/db-pages/'
+                 */
+		if (substr($node['url_href'], 0, 10) == '/db-pages/') {
+			try
+			{
+				//print_r(
+						//strtolower($_GET['page']) . '.html'
+						//.
+						       //substr($node['url_href'], 11)
+					//);
+				$cmf = HaddockProjectOrganisation_ConfigManagerFactory::get_instance();
+				$config_manager = $cmf->get_config_manager('plug-ins', 'db-pages');
+				if ($page_class_str == $config_manager->get_html_page_class_name()) {
+					if (
+						strtolower($_GET['page']) . '.html'
+					       	==
+					       	substr($node['url_href'], 10)
+					) {
+						return TRUE;
+					}
+				}
+			}
+			catch (Exception $e)
+			{
+
+			}
+		}	
+		return FALSE;
+
+	}
+
 	
 	public static function
 		get_project_specific_1d_ul(
