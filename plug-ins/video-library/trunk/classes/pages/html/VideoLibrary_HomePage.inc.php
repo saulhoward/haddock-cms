042573fff77062d379d72538e075ae3aae88ee98
saulhoward <saulhoward@11343cde-aa41-0410-9af0-57f1f6311122>
2009-11-19 07:04:04 +0000

    fixed home page

--- plug-ins/video-library/trunk/classes/pages/html/VideoLibrary_HomePage.inc.php
+++ plug-ins/video-library/trunk/classes/pages/html/VideoLibrary_HomePage.inc.php
@@ -17,11 +17,30 @@ VideoLibrary_ThumbnailsPage
 		set_videos()
 	{
 		$videos = VideoLibrary_DatabaseHelper::get_external_videos(
-			$this->get_external_video_library_id()
+                        $this->get_external_video_library_id(),
+                        $this->get_start(),
+                        $this->get_duration()
 		);
 
 		$this->videos = $videos;
-	
 	}
+
+        protected function
+                set_total_videos_count()
+        {
+                $count = VideoLibrary_DatabaseHelper::
+                        get_external_videos_count(
+                                $this->get_external_video_library_id()
+                        );
+                $this->total_videos_count = $count;
+        }
+
+        protected function
+                get_page_url()
+        {
+                /* Pretend to be the search page for links to work */
+                return VideoLibrary_URLHelper::
+                        get_search_page_url();
+        }
 }
 ?>
