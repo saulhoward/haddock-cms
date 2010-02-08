f483a92f423f4591306b70bed6a2d806beb8dcec
robert.impey <robert.impey@11343cde-aa41-0410-9af0-57f1f6311122>
2010-02-08 12:48:28 +0000

    Tagged the textile plug-in.

---
+++ plug-ins/textile/tags/2010-02-08/classes/helpers/Textile_TranslationHelper.inc.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Textile_TranslationHelper
+ *
+ * @copyright 2009-02-08, Robert Impey
+ */
+
+class
+	Textile_TranslationHelper
+{
+	public static function
+		translate_textile_to_html($textile_text)
+	{
+		$textile_text = stripslashes($textile_text);
+		$textile = new Textile_Textile();
+		
+		return $textile->TextileThis($textile_text);
+	}
+}
+?>
