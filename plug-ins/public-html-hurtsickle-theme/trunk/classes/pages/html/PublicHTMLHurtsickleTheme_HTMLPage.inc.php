af488d63f12242ccc251471fad877d189a8ef98b
robert.impey <robert.impey@11343cde-aa41-0410-9af0-57f1f6311122>
2010-02-08 12:02:18 +0000

    Added the parent class for HTML page classes that implement this theme.

---
+++ plug-ins/public-html-hurtsickle-theme/trunk/classes/pages/html/PublicHTMLHurtsickleTheme_HTMLPage.inc.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * PublicHTMLHurtsickleTheme_HTMLPage
+ *
+ * @copyright 2010-02-08, Robert Impey
+ */
+
+abstract class
+	PublicHTMLHurtsickleTheme_HTMLPage
+extends
+	PublicHTML_HTMLPage
+{
+}
+?>
