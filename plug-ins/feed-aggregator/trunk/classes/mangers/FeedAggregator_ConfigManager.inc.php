7a5fde8d359bd7d4fb88494b027bc0468fdda086
saulhoward <saulhoward@11343cde-aa41-0410-9af0-57f1f6311122>
2010-03-19 15:23:38 +0000

    new manager class for the fee-agg config

---
+++ plug-ins/feed-aggregator/trunk/classes/mangers/FeedAggregator_ConfigManager.inc.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * FeedAggregator_ConfigManager
+ *
+ * @copyright 2010-03-19, SANH
+ */
+
+class
+	FeedAggregator_ConfigManager
+extends
+	HaddockProjectOrganisation_ConfigManager
+{
+	protected function
+		get_module_prefix_string()
+	{
+		return '/plug-ins/feed-aggregator/';
+	}
+	
+	public function
+		get_page_builder_class_name()
+	{
+		return trim($this->get_default_feed_retrieval_frequency_in_minutes('page-building/default-feed-retrieval-frequency-in-minutes'));
+	}
+}
+?>
