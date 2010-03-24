8552ec3b53d978f31e46e9e8a5d3c90976122e06
saulhoward <saulhoward@11343cde-aa41-0410-9af0-57f1f6311122>
2010-03-24 14:24:44 +0000

    config variable for the parser used

--- plug-ins/feed-aggregator/trunk/sql/hpi_feed_aggregator_retrieval_queue.sql
+++ plug-ins/feed-aggregator/trunk/sql/hpi_feed_aggregator_retrieval_queue.sql
@@ -1,7 +1,13 @@
+-- 
+-- Table structure for table `hpi_feed_aggregator_retrieval_queue`
+-- 
+
 CREATE TABLE `hpi_feed_aggregator_retrieval_queue` (
-    `id` INT( 10 ) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-    `feed_id` INT( 10 ) UNSIGNED NOT NULL ,
-    `frequency_minutes` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_roman_ci NOT NULL ,
-    `last_retrieved` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_roman_ci NULL ,
-    `status` ENUM( 'retrieve', 'skip' ) NOT NULL DEFAULT 'skip'
-) ENGINE = MYISAM ;
+    `id` int(10) unsigned NOT NULL auto_increment,
+    `feed_id` int(10) unsigned NOT NULL,
+    `frequency_minutes` varchar(255) character set utf8 collate utf8_roman_ci NOT NULL,
+    `last_retrieved` datetime default NULL,
+    `status` enum('retrieve','skip') NOT NULL default 'skip',
+    PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
