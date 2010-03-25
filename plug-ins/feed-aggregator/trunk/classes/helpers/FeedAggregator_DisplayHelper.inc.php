a4f121c169b75006a60b7cf8364e672b0b8f35ee
saulhoward <saulhoward@11343cde-aa41-0410-9af0-57f1f6311122>
2010-03-25 11:09:53 +0000

    display heper

---
+++ plug-ins/feed-aggregator/trunk/classes/helpers/FeedAggregator_DisplayHelper.inc.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * FeedAggregator_DisplayHelper
+ *
+ * @copyright 2010-03-19, SANH
+ */
+
+class
+FeedAggregator_DisplayHelper
+{
+	public static function
+        get_feed_summaries_div(
+            $tags,                     // array
+            $number_of_feeds,
+            $number_of_items_per_feed
+        )
+    {
+
+    }
+	
+
+}
+?>
