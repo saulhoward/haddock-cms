8218ab5a78da4505eb4ed6d7657b28ef79a174bc
saulhoward <saulhoward@11343cde-aa41-0410-9af0-57f1f6311122>
2010-04-01 10:16:54 +0000

    adjusted stupid limit on db field size

--- plug-ins/feed-aggregator/trunk/sql/hpi_feed_aggregator_cache.sql
+++ plug-ins/feed-aggregator/trunk/sql/hpi_feed_aggregator_cache.sql
@@ -7,11 +7,11 @@ CREATE TABLE `hpi_feed_aggregator_cache` (
     `feed_id` int(10) unsigned NOT NULL,
     `unique_item_id` varchar(255) character set utf8 collate utf8_roman_ci NOT NULL,
     `date_retrieved` varchar(255) character set utf8 collate utf8_roman_ci NOT NULL,
-    `full_content` varchar(255) character set utf8 collate utf8_roman_ci NOT NULL,
+    `full_content` varchar(1024) character set utf8 collate utf8_roman_ci NOT NULL,
     `title` varchar(255) character set utf8 collate utf8_roman_ci NOT NULL,
     `link` varchar(255) character set ucs2 collate ucs2_roman_ci NOT NULL,
     `updated` varchar(255) character set utf8 collate utf8_roman_ci NOT NULL,
-    `summary` varchar(255) character set utf8 collate utf8_roman_ci NOT NULL,
+    `summary` varchar(1024) character set utf8 collate utf8_roman_ci NOT NULL,
     PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
