6e9e58b2abd05b89b8ae9f399189cc5e24c07cf1
saulhoward <saulhoward@11343cde-aa41-0410-9af0-57f1f6311122>
2010-04-01 07:44:14 +0000

    added simjple pie library to helper plugin

--- plug-ins/simple-pie/trunk/classes/SimplePie_SimplePie.inc.php
+++ plug-ins/simple-pie/trunk/classes/SimplePie_SimplePie.inc.php
@@ -3,7 +3,15 @@
  * SimplePie_SimplePie
  * 
  * @copyright SANH, 2010-03-24
+ *
+ * A class to extend the SimplePie library so I can use it in
+ * the haddock style
+ *
+ * uses a require statement to get the library (which needs to be kept 
+ * in the same directory)
  */
+
+require_once('simplepie.inc');
 
 class
 SimplePie_SimplePie
