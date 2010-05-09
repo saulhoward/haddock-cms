7bfb18d35e3fa665b84148074fdd35e4129441f5
robert.impey <robert.impey@11343cde-aa41-0410-9af0-57f1f6311122>
2010-05-09 17:27:24 +0000

    Tag for today.

---
+++ plug-ins/public-html-sky-theme/tags/2010-05-09_1827/classes/pages/html/PublicHTMLSkyTheme_HTMLPage.inc.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * PublicHTMLSkyTheme_HTMLPage
+ *
+ * @copyright 2009-02-03, Robert Impey
+ */
+
+abstract class
+	PublicHTMLSkyTheme_HTMLPage
+extends
+	PublicHTML_HTMLPage
+{
+	protected function
+		render_head_link_stylesheet()
+	{
+		HTMLTags_LinkRenderer
+			::render_style_sheet_link(
+				'/plug-ins/public-html-sky-theme/public-html/styles/layout.css'
+			);
+		HTMLTags_LinkRenderer
+			::render_style_sheet_link(
+				'/plug-ins/public-html-sky-theme/public-html/styles/type-face.css'
+			);
+		HTMLTags_LinkRenderer
+			::render_style_sheet_link(
+				'/plug-ins/public-html-sky-theme/public-html/styles/rounded-corners.css'
+			);
+		
+?>
+<!--[if lt IE 7.]>
+<link
+	href="/plug-ins/public-html-sky-theme/public-html/styles/gifs-for-ie6.css"
+	rel="stylesheet"
+	type="text/css"
+/>
+<![endif]-->
+<?php
+	}
+		
+	public function
+		render_head_script_javascript()
+	{
+		HTMLTags_ScriptRenderer
+			::render_external_js_script(
+				'/plug-ins/public-html-sky-theme/public-html/scripts/rounded-corners.js'
+			);
+		HTMLTags_ScriptRenderer
+			::render_external_js_script(
+				'/plug-ins/run-on-load/public-html/scripts/run-on-load.js'
+			);
+		HTMLTags_ScriptRenderer
+			::render_external_js_script(
+				'/plug-ins/public-html-sky-theme/public-html/scripts/register-functions-to-run-on-load.js'
+			);
+	}
+	
+	public function
+		render_body()
+	{
+		$this->render_body_tag_open();
+		
+		echo "<div id=\"container\">\n";
+		
+		$this->render_body_div_header();
+		
+		$this->render_body_div_content();
+		
+		$this->render_body_div_navigation();
+		
+		$this->render_body_div_footer();
+		
+		echo "</div>\n";
+		
+		echo "</body>\n";
+	}
+	
+	public function
+		get_body_div_header()
+	{
+		$div_header = parent::get_body_div_header();
+		
+		$div_header->set_attribute_str('class', 'bw-rc');
+		
+		return $div_header;
+	}
+	
+	public function
+		render_body_div_content()
+	{
+?>
+<div id="content" class="bw-rc">
+<?php
+		$this->content();
+?>
+</div>
+<!-- End of content div -->
+<?php
+	}
+	
+	public function
+		render_body_div_navigation()
+	{
+		echo '<div id="navigation" class="bw-rc">' . PHP_EOL;
+		$this->render_content_of_body_div_navigation();
+		echo '</div>' . PHP_EOL;
+	}
+	
+#	public function
+#		render_html()
+#	{
+#		parent::render_html();
+#
+#<script type="text/javascript">
+#round_all_corners('bw-rc');
+#</script>
+#
+#	}
+}
+?>
---
+++ plug-ins/public-html-sky-theme/tags/2010-05-09_1827/public-html/scripts/rounded-corners.js
@@ -0,0 +1,97 @@
+/**
+ * Functions for wrapping a div with Round corners.
+ *
+ * @copyright 2008-01-24, Robert Impey
+ */
+
+function round_all_bw_rc_corners()
+{
+	round_all_corners('bw-rc');
+}
+
+function round_all_corners(class_name)
+{
+	var divs_to_round = document.getElementsByTagName('div');
+
+	for (var i = 0; i < divs_to_round.length; i++) {
+		var current_div = divs_to_round.item(i);
+		
+		if (current_div.className == class_name) {
+			round_corners(current_div);
+		}
+	}
+}
+
+function round_corners(current_div)
+{
+	/*
+	 * Cut the existing content of the div.
+	 */
+	currrent_div_inner_html = current_div.innerHTML;
+	
+	current_div.innerHTML = '';
+	
+	/*
+	 * The Header.
+	 */
+	var div_h = document.createElement('div');
+	div_h.className = 'row h horiz';
+
+	var div_h_l = document.createElement('div');
+	div_h_l.className = 'l corner';
+	var div_h_c = document.createElement('div');
+	div_h_c.className = 'c';
+	var div_h_r = document.createElement('div');
+	div_h_r.className = 'r corner';
+	
+	div_h.appendChild(div_h_l);
+	div_h_l.appendChild(div_h_r);
+	div_h_r.appendChild(div_h_c);
+	
+	current_div.appendChild(div_h);
+	
+	/*
+	 * The middle.
+	 */
+	
+	var div_m = document.createElement('div');
+	div_m.className = 'row m';
+
+	var div_m_l = document.createElement('div');
+	div_m_l.className = 'l vert';
+	var div_m_c = document.createElement('div');
+	div_m_c.className = 'c';
+	var div_m_r = document.createElement('div');
+	div_m_r.className = 'r vert';
+
+	/*
+	 * Paste the previous contents into this inner div.
+	 */
+	div_m_c.innerHTML = currrent_div_inner_html;
+
+	div_m.appendChild(div_m_l);
+	div_m_l.appendChild(div_m_r);
+	div_m_r.appendChild(div_m_c);
+	
+	current_div.appendChild(div_m);
+	
+	/*
+	 * The footer.
+	 */
+	
+	var div_f = document.createElement('div');
+	div_f.className = 'row f horiz';
+
+	var div_f_l = document.createElement('div');
+	div_f_l.className = 'l corner';
+	var div_f_c = document.createElement('div');
+	div_f_c.className = 'c';
+	var div_f_r = document.createElement('div');
+	div_f_r.className = 'r corner';
+	
+	div_f.appendChild(div_f_l);
+	div_f_l.appendChild(div_f_r);
+	div_f_r.appendChild(div_f_c);
+	
+	current_div.appendChild(div_f);
+}
