41fd2b8aa257c660a142a5b6c409400f09d7a8f0
saulhoward <saulhoward@11343cde-aa41-0410-9af0-57f1f6311122>
2010-05-19 09:33:44 +0000

    added ability to use your own seletion function in nav selected li ul

--- plug-ins/navigation/trunk/classes/helpers/Navigation_HTMLListsHelper.inc.php
+++ plug-ins/navigation/trunk/classes/helpers/Navigation_HTMLListsHelper.inc.php
@@ -62,55 +62,45 @@ class
 			$page_class_str = NULL
 		)
 	{
-		if (!isset($class_name)) {
-			$class_name = 'navigation';
-		}
-		
-		$nodes
-			= Navigation_1DTreeRetriever
-				::get_tree_nodes($tree_name);
-				
-		#print_r($nodes);
-		
-		#echo "<ul class=\"$class_name\">\n";
-		
-		$ul = new HTMLTags_UL();
-		
-		$ul->set_class($class_name);
-		
-		foreach ($nodes as $node) {
-			#Navigation_NodeRenderer::render_node($node);
-			
-			$li = new HTMLTags_LI();
-			$li->set_attribute_str(
-				'id',
-				self::get_line_css_id($node['url_href'])
-			);
-
-			if (
-				($page_class_str != NULL)
-				&& 
-				(self::is_node_selected($node, $page_class_str))
-			)	{
-				$li->set_class('selected');
-			}
-			$li->append(
-				Navigation_NodesHelper
-					::get_link_a_with_span($node)
-			);
-			
-			$ul->add_li(
-				$li
-			);
-		}
-		
-		return $ul;
+        return self::get_1d_ul_with_selected_lines(
+            $tree_name,
+            $class_name,
+            $page_class_str,
+            array(
+                'use_span' => TRUE
+            )
+        );
 	}
+
 	public static function
 		get_1d_ul_with_selected_lines(
 			$tree_name,
 			$class_name = NULL,
-			$page_class_str = NULL
+            $page_class_str = NULL,
+            $options = array(
+                'use_span' => FALSE
+            )
+		)
+	{
+        return self::get_1d_ul_with_selected_lines_using_custom_function(
+            $tree_name,
+            $class_name,
+            $page_class_str,
+            $selected_function = 'return Navigation_HTMLListsHelper::is_node_selected($node, $page_class_str);',
+            $options
+        );
+
+	}
+
+	public static function
+		get_1d_ul_with_selected_lines_using_custom_function(
+			$tree_name,
+			$class_name = NULL,
+            $page_class_str = NULL,
+            $selected_function = 'return Navigation_HTMLListsHelper::is_node_selected($node, $page_class_str);',
+            $options = array(
+                'use_span' => FALSE
+            )
 		)
 	{
 		if (!isset($class_name)) {
@@ -138,18 +128,27 @@ class
 				self::get_line_css_id($node['url_href'])
 			);
 
+            // print_r($selected_function);exit;
+
 			if (
 				($page_class_str != NULL)
 				&& 
-				(self::is_node_selected($node, $page_class_str))
+				(eval($selected_function))
 			)	{
 				$li->set_class('selected');
 			}
 			
-			$li->append(
-				Navigation_NodesHelper
-					::get_link_a($node)
-			);
+            if ($options['use_span']) {
+                $li->append(
+                    Navigation_NodesHelper
+                    ::get_link_a_with_span($node)
+                );
+            } else {
+                $li->append(
+                    Navigation_NodesHelper
+                    ::get_link_a($node)
+                );
+            }
 			
 			$ul->add_li(
 				$li
@@ -164,12 +163,12 @@ class
 			$url_href
 		)
 	{
-                /**
-		 * Guess the most appropriate id attribute to give each line
-		 * of the ul -- they need to be unique, so we're gonna start
-		 * by using the url_href
-                 */
-		$url_href = strtolower(str_replace(' ', '', $url_href));
+        /**
+         * Guess the most appropriate id attribute to give each line
+         * of the ul -- they need to be unique, so we're gonna start
+         * by using the url_href
+         */
+        $url_href = strtolower(str_replace(' ', '', $url_href));
 		if ($url_href == '/') {
 		       	return 'home';
 		} elseif (strpos($url_href, '.html')) {
@@ -190,19 +189,20 @@ class
 			$page_class_str
 		)
 	{
-                /**
-		 * This is a best guess attempt at whether this node is the same 
-		 * as the page we're currently on. Based on various guesses about
-		 * plug-ins and normal practices, it  seems to work so far...
-                 */
-		//print_r($node);print_r($page_class_str);
+        //print_r($node);print_r($page_class_str);
 		//print_r($_GET);
 		//print_r($_SERVER['SCRIPT_NAME']);exit;
 		//print_r($node);print_r($page_class_str);exit;
 
-                /*
+        /**
+		 * This is a best guess attempt at whether this node is the same 
+		 * as the page we're currently on. Based on various guesses about
+		 * plug-ins and normal practices, it  seems to work so far...
+         */
+
+        /*
 		 *Home Page, if it's the Default page and the href is '/'
-                 */
+         */
 		if ($node['url_href'] == '/') {
 			$cmf = HaddockProjectOrganisation_ConfigManagerFactory::get_instance();
 			$pm = $cmf->get_config_manager('haddock', 'public-html');
@@ -218,14 +218,14 @@ class
 			return TRUE;
 		}
 
-                /**
+        /**
 		 * Site Texts plug in. 
 		 * At the mo, ['site-texts'] & ['language']
 		 * get info and the .htaccess rule which translates 
 		 * them from /en/home.html or /fr/home.html
 		 * is only set in one certain proj-spec,
 		 * but I wanna move it up so I included this code here
-                 */
+         */
 		if (isset($_GET['site-texts'])) {
 			if (
 				isset($_GET['language'])
@@ -253,9 +253,9 @@ class
 			}
 		}
 
-                /*
+        /*
 		 *DB Page, if the url starts with '/db-pages/'
-                 */
+         */
 		if (substr($node['url_href'], 0, 10) == '/db-pages/') {
 			try
 			{
