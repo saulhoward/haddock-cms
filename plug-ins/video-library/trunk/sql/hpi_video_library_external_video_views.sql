85e5f0c8b8a14ceca48c33d57854fb4d771dabaf
saulhoward <saulhoward@11343cde-aa41-0410-9af0-57f1f6311122>
2010-06-27 09:33:39 +0000

    changed table to use unix time

--- plug-ins/video-library/trunk/sql/hpi_video_library_external_video_views.sql
+++ plug-ins/video-library/trunk/sql/hpi_video_library_external_video_views.sql
@@ -6,8 +6,8 @@ CREATE TABLE `hpi_video_library_external_video_views` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `external_video_id` int(10) unsigned NOT NULL,
   `views` int(10) unsigned NOT NULL default '0',
-  `from` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-  `to` timestamp NOT NULL default '0000-00-00 00:00:00',
+  `from`  int(10) unsigned NOT NULL,
+  `to`  int(10) unsigned NOT NULL,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `external_video_id` (`external_video_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
