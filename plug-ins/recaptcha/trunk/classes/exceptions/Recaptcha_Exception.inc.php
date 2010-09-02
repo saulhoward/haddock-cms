94b2cdb22225339a86006c11cba22f77337bd4c3
saulhoward <saulhoward@11343cde-aa41-0410-9af0-57f1f6311122>
2010-09-02 09:19:33 +0000

    wrapper for check answer library funtion

---
+++ plug-ins/recaptcha/trunk/classes/exceptions/Recaptcha_Exception.inc.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Recaptcha_Exception
+ *
+ * @copyright 2009-10-02, SANH
+ */
+
+class
+Recaptcha_Exception
+extends
+Exception
+{
+	public function
+		__construct($msg)
+	{
+		parent::__construct($msg);
+	}
+
+}
+?>
