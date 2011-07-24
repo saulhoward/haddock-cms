981fa3000a32ab5d5f204e12a4502c8aaaffa987
robert.impey <robert.impey@11343cde-aa41-0410-9af0-57f1f6311122>
2011-07-24 21:02:06 +0000

    Links to the user log in and mailing list pages.

--- sites/haddock-sanity-check/trunk/classes/pages/html/HaddockSanityCheck_HTMLPage.inc.php
+++ sites/haddock-sanity-check/trunk/classes/pages/html/HaddockSanityCheck_HTMLPage.inc.php
@@ -10,5 +10,31 @@ abstract class
 extends
 	PublicHTML_HTMLPage
 {
+    	protected function
+		get_navigation_pages()
+	{
+		$navigation_pages[] = array(
+			'page-class' => 'HaddockSanityCheck_HomeHtmlPage',
+			'href' => '/HaddockSanityCheck_HomeHtmlPage',
+			'title' => 'Home',
+			'text' => 'Home'
+		);
+		
+		$navigation_pages[] = array(
+			'page-class' => 'MailingList_SignUpPage',
+			'href' => '/MailingList_SignUpPage',
+			'title' => 'Mailing List Sign up Page',
+			'text' => 'Mailing List Sign up Page'
+		);
+		
+		$navigation_pages[] = array(
+			'page-class' => 'UserLogin_RegistrationPage',
+			'href' => '/UserLogin_RegistrationPage',
+			'title' => 'User Login Registration Page',
+			'text' => 'User Login Registration Page'
+		);
+		
+		return $navigation_pages;
+	}
 }
 ?>
--- sites/haddock-sanity-check/trunk/classes/pages/html/HaddockSanityCheck_HomeHtmlPage.inc.php
+++ sites/haddock-sanity-check/trunk/classes/pages/html/HaddockSanityCheck_HomeHtmlPage.inc.php
@@ -9,6 +9,11 @@ extends
     {
 ?>
 <h2>Home</h2>
+
+<p>
+The purpose of this site is to test the core of Haddock CMS and the various plug-ins
+for it.
+</p>
 <?php
     }
 }
